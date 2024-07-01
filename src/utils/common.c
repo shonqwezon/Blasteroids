9e877f775e904683a68ccc1da84834cc33dda92a
Shon Qwezon <shonzbso@gmail.com>
2024-07-01 14:41:22 +0300

    Add concat func in `common`

---
+++ src/utils/common.c
@@ -0,0 +1,14 @@
+//
+// Created by Shon on 01.07.2024.
+//
+
+#include "common.h"
+#include <string.h>
+#include <stdlib.h>
+
+char *concat(const char *str1, const char *str2) {
+    char *res = malloc(strlen(str1) + strlen(str2) + 1);
+    strcpy(res, str1);
+    strcat(res, str2);
+    return res;
+}
---
+++ src/utils/common.h
@@ -0,0 +1,10 @@
+//
+// Created by Shon on 01.07.2024.
+//
+
+#ifndef BLASTEROIDS_COMMON_H
+#define BLASTEROIDS_COMMON_H
+
+char *concat(const char *str1, const char *str2);
+
+#endif //BLASTEROIDS_COMMON_H
