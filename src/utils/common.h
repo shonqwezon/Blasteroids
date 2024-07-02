c29eb1e61ee961f91d83768fbcd5f9b1c2fe6e14
Shon Qwezon <shonzbso@gmail.com>
2024-07-02 13:24:43 +0300

    Move EventInfo to common.h

--- src/utils/common.h
+++ src/utils/common.h
@@ -5,6 +5,14 @@
 #ifndef BLASTEROIDS_COMMON_H
 #define BLASTEROIDS_COMMON_H
 
+#include <allegro5/allegro.h>
+
 char *concat(const char *str1, const char *str2);
+
+typedef struct {
+    bool isRun;
+    bool keys[ALLEGRO_KEY_MAX];
+    ALLEGRO_DISPLAY *display;
+} EventInfo;
 
 #endif //BLASTEROIDS_COMMON_H
